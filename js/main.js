49674e18ade34e0991bedcafa62428a5e03cf5a2
BOSUN KIM <bosunkim@BOSUNs-iMac.local>
2021-05-30 22:58:48 -0700

    card deck template and image update

--- js/main.js
+++ js/main.js
@@ -1,118 +1,169 @@
-
-const startButton = document.getElementById('start-game');
-
-console.log(bgImg.style.backgroundImage);
-
-
-Pseudocode
-
+//Pseudocode
 //define constant variable:
-
 const people = {
 	tombo: {
-		  name: ‘Tombo’,
-		  imgUrl: ‘someUrl.png’
-		},
+		name: 'Tombo',
+		imgUrl: '/img/Tombo.png'
+	},
 	ursula: {
-		name:,
-		imgUrl:
-		},
+		name: 'Ursula',
+		imgUrl: '/img/Ursula.png'
+	},
 	ket: {
-		name:,
-		imgUrl:
-		},
+		name: 'Ket',
+		imgUrl: '/img/Ket.png'
+	},
 	osono: {
-		name:,
-		imgUrl:
-		},
+		name: 'Osono',
+		imgUrl: '/img/Osono.png'
+	},
 	maki: {
-		name:,
-		imgUrl:
-		},
+		name: 'Maki',
+		imgUrl: '/img/Maki.png'
+	},
 	police: {
-		name:,
-		imgUrl:
-		}
+		name: 'Police',
+		imgUrl: '/img/Police.png'
 	}
-
-const items = {
-	hamburger: {},
-	mail: {},
-	bread: {},
-	milk: {},
-	secretDocument: {},
-	magicPotion: {},
-	newsPaper: {},
-	flowers: {},
-	present: {},
-	toy: {},
-	key: {},
-	bentoBox: {}
 }
 
-const cardDeck = [ ‘tombo’, ‘ursula’, ‘ket’, ‘osono’, ‘maki’, ‘police’, ‘hamburger’, ‘mail’, ‘bread’, ‘milk’, ‘secretDocument’, ‘magicPotion’, ‘newsPaper’, ‘flowers’, ‘present’, ‘toy’, ‘key’, ‘bentoBox’];
+const items = {
+	hamburger: {
+		name: 'Hamburger',
+		imgUrl: '/img/hamburger.png'
+	},
+	mail: {
+		name: 'Mail',
+		imgUrl: '/img/Mail.png'
+	},
+	bread: {
+		name: 'Bread',
+		imgUrl: '/img/Bread.png'
+	},
+	milk: {
+		name: 'Milk',
+		imgUrl: '/img/Milk.png'
+	},
+	secretDocument: {
+		name: 'Secret Document',
+		imgUrl: '/img/SecretDocument.png'
+	},
+	magicPotion: {
+		name: 'Magic Potion',
+		imgUrl: '/img/MagicPotion.png'
+	},
+	newsPaper: {
+		name: 'News Paper',
+		imgUrl: '/img/NewsPaper.png'
+	},
+	flowers: {
+		name: 'Flowers',
+		imgUrl: '/img/Flowers.png'
+	},
+	present: {
+		name: 'Present',
+		imgUrl: '/img/Present.png'
+	},
+	toy: {
+		name: 'Toy',
+		imgUrl: '/img/Toy.png'
+	},
+	key: {
+		name: 'Key',
+		imgUrl: '/img/Key.png'
+	},
+	bentoBox: {
+		name: 'Bento Box',
+		imgUrl: '/img/BentoBox.png'
+	}
+}
 
-
+const cardDeck = ['tombo', 'ursula', 'ket',
+'osono', 'maki', 'police',
+'hamburger', 'mail', 'bread',
+'milk', 'secretDocument', 'magicPotion',
+'newsPaper', 'flowers', 'present',
+'toy', 'key', 'bentoBox'];
 
 
 
 //define state variable:
 let currentLocation = {
 	//when the game is newly generated, this variable needs to be updated
-	};
+};
 
 let targetList = {
 	// the target needs to be generated each time the game restarts.
-	first: {
-	    people: ‘name’,
-	    item: ‘name’
-	},
-	second: {
-	    people:,
-	    item:
-	},
-	third: {
-	    people:,
-	    item:
-	}
+// 	first: {
+// 		people: ‘name’,
+// 	    item: ‘name’
+// 	},
+// 	second: {
+// 		people:,
+// 	    item:
+// 	},
+// 	third: {
+// 		people:,
+// 	    item:
+// 	}
 }
 
-let currentPlayerChoice= {
+//let currentPlayerChoice= {
 	//when player clicks card, this variable needs to be updated
 	//only one set of target (a person, an item) can be stored 
 	//once the set is correct, this value needs to be pushed(or other object method similar to array.push*) to matchedList
-	}  //is object better? or array better in this case?
+//}  //is object better? or array better in this case?
 
 let matchedList = {}; //starts as empty object, will be updated as the new match happens
-	
+
 let level; //shows current level. Once all the objects in the targetList are matched, this value will increase by 1.
 
-let chances: //shows how many chances left. Each time mismatch happens, this value decreases by 1.
+let chances; //shows how many chances left. Each time mismatch happens, this value decreases by 1.
 
 
 
 
-	
+
 //cached element reference:
 
 
+const startButton = document.getElementById('start-game');
+const logo = document.getElementById('logo');
+const landing = document.querySelector('.landing');
+const gameBoard = document.querySelector('.game-board');
 
-cardDeck; front and back. When the game starts, all the cardDeck should show the ‘back’ side image
-when the player choice is matching any of targetList, then highlight (box-shadow) the cards on the DOM - this will stay showing ‘face’ side up until the next game
-chances - (either heart or star) 
-level
-delivery list comment (targetList)
-check mark image
+// cardDeck; front and back. When the game starts, all the cardDeck should show the ‘back’ side image
+// when the player choice is matching any of targetList, then highlight (box-shadow) the cards on the DOM - this will stay showing ‘face’ side up until the next game
+// chances - (either heart or star) 
+// level
+// delivery list comment (targetList)
+// check mark image
 
-*extra = will provide extra (possibly pop-up page) document to show people’s face and name - for the reference
+// *extra = will provide extra (possibly pop-up page) document to show people’s face and name - for the reference
 
 
 //event listeners:
 
-cardDeck -> when it’s clicked then it will trigger the matching function & winning condition
-*extra help-icon which will generate pop-up for the reference
+// cardDeck -> when it’s clicked then it will trigger the matching function & winning condition
+// *extra help-icon which will generate pop-up for the reference
 
 //functions:
+
+startButton.addEventListener('click', function(e) {
+	console.log('click is working!');
+	logo.style.display = 'none';
+	startButton.style.display = 'none';
+	for(let i = 0; i < 18; i++){
+		const cardDeck = document.createElement('div');
+		cardDeck.innerText = i;
+		cardDeck.className = 'card-deck';
+		gameBoard.appendChild(cardDeck);
+	}
+
+})
+
+
+
 
 function init() {
 	// invoke mixingCard() function when the game resets
